12f6dfcad68eac06fe4408e5f5d638ec962acefa
Tobias Markus <markus@dantronik.de>
2020-07-09 12:18:27 +0200

    Projektdateien hinzufügen.

---
+++ SelectBindingBug/Server/Controllers/WeatherForecastController.cs
@@ -0,0 +1,42 @@
+﻿using SelectBindingBug.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SelectBindingBug.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                SelectedIndex = rng.Next(2) == 0 ? rng.Next(Summaries.Length) : (decimal?)null,
+                WeatherItems = Summaries.ToDictionary(a => (decimal?)Summaries.ToList().IndexOf(a), a => a),
+                Valid = rng.Next(2)
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ SelectBindingBug/Shared/WeatherForecast.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SelectBindingBug.Shared
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public string Summary { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public decimal? SelectedIndex { get; set; }
+
+        public Dictionary<decimal?, string> WeatherItems { get; set; }
+
+        public decimal? Valid { get; set; } 
+    }
+}
